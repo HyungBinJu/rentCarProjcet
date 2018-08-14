bcb366e9c8a049e1597577abe82ffde8cd2073d1
Haeun Lee <cherrypoco@naver.com>
2018-08-14 19:01:18 +0900

    ㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛㅛ

--- rentCarProject/src/com/reservation/SelectDateCarServlet.java
+++ rentCarProject/src/com/reservation/SelectDateCarServlet.java
@@ -79,9 +79,6 @@ HttpSession session = request.getSession();
 		dis.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
